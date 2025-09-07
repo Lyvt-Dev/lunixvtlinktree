06f407872642484bd2a85e90676126273171a561
Lyvt <lyvt@proton.me>
2025-09-08 00:31:28 +0200

    Add files via upload

---
+++ assets/javascript/twitch-config.js
@@ -0,0 +1,11 @@
+
+
+window.TWITCH_CLIENT_ID = 'YOUR_CLIENT_ID_HERE'; 
+window.TWITCH_ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN_HERE';
+window.TWITCH_CHANNEL_NAME = 'lunixvt';
+
+window.X_USERNAME = 'lunixvt';
+
+window.X_TWEET_URLS = [
+    'https://twitter.com/lunixVT/status/1962337587604992274'
+];
